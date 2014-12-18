dcb74b6cb38bf9995e0fe99149738c524c6a8cbd
Don Irwin <don@falconwebtech.com>
2014-12-17 18:54:27 -0700

    Create VisiCalc.java

---
+++ src/VisiCalc.java
@@ -0,0 +1,17 @@
+public class VisiCalc {
+
+	public static void main(String[] args) {
+		// create an array of cells
+
+		
+		// initialize the array
+
+
+		
+		// add test values
+
+		
+		// print the array
+
+	}
+}

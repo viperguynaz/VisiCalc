989909c0f90980cf12905535272eab60c29a7c3f
viperguynaz <don@falconwebtech.com>
2015-01-08 21:37:21 -0700

    CheckPoint #2
    Complete for CP2

--- src/VisiCalc.java
+++ src/VisiCalc.java
@@ -1,51 +1,37 @@
-public class VisiCalc {
+public class VisiCalc {  
 
-  public static String padOrTruncateDisplayString(Cell cell) {
-    // truncate string to length - 1 and display >
-    int columnWidth = 12;
-    String s = cell.getDisplayString();
-    if (s.length() > columnWidth) {
-      s = s.substring(0, columnWidth - 1) + ">";
-    }
-    // if length is odd pad end to even length
-    if (s.length() % 2 == 1) {
-      s += " ";
-    }
-    // justify pad to center in cell
-    while (s.length() < columnWidth) {
-      s = " " + s + " ";
-    }
-    return s;
-  }
-  
-  
- public static void main(String[] args) {
-  // create an array of cells
-  Cell[] cells = new Cell[12];
-  
-  // add test values
-  cells[0] = new CellString("");
-  cells[1] = new CellString("wow");
-  cells[2] = new CellString("quot\"es");
-  cells[3] = new CellString("wow this is a long string");  
-  
-  cells[4] = new CellNumeric();
-  cells[5] = new CellNumeric("abc");  // handle parse error, should result in empty value
-  cells[6] = new CellNumeric("105.12");
-  cells[7] = new CellNumeric("3.14159265359");
-  
-  cells[8] =  new CellDate();
-  cells[9] =  new CellDate("43");  // handle parse error, should result in empty value
-  cells[10] = new CellDate("1/1/1990");
-  cells[11] = new CellDate("12/25/2014");
-  
-  // print the array
-  String rowBorder = "------------|------------|------------";
-  System.out.println(padOrTruncateDisplayString(new CellString("row")) + "|" + padOrTruncateDisplayString(new CellString("input")) + "|" + padOrTruncateDisplayString(new CellString("output")));
-  System.out.println(rowBorder);
-  for (int i = 0; i < cells.length; i++) {
-   System.out.println(padOrTruncateDisplayString(new CellString(""+(i+1))) + "|" + padOrTruncateDisplayString(cells[i]) + "| " + cells[i].getInputValue());
-   System.out.println(rowBorder);
-  }
- }
+	public static void main(String[] args) {
+		// create a spreadsheet and print it
+		Spreadsheet sp = new Spreadsheet();
+		sp.print();
+		System.out.println();
+
+		// set some cells and print spreadsheet
+
+		sp.setCell("A1", new CellString("wow"));
+		sp.setCell("B2", new CellString("quot\"es"));
+		sp.setCell("C3", new CellString("wow this is a long string"));		
+
+		sp.setCell("D4", new CellNumeric("abc"));  // handle parse error, should result in empty value
+		sp.setCell("E5", new CellNumeric("105.12"));
+		sp.setCell("F6", new CellNumeric("3.14159265359"));
+
+		sp.setCell("G7", new CellDate("43"));  // handle parse error, should result in empty value
+		sp.setCell("A3", new CellDate("1/1/1990"));
+		sp.setCell("B4", new CellDate("12/25/2014"));
+		
+		System.out.println();
+		sp.print();
+		System.out.println();
+		
+		System.out.println("A1 = " + sp.getCell("A1").getInputValue());
+		System.out.println("B2 = " + sp.getCell("B2").getInputValue());
+		System.out.println("C3 = " + sp.getCell("C3").getInputValue());
+		System.out.println("D4 = " + sp.getCell("D4").getInputValue());
+		System.out.println("E5 = " + sp.getCell("E5").getInputValue());
+		System.out.println("F6 = " + sp.getCell("F6").getInputValue());
+		System.out.println("G7 = " + sp.getCell("G7").getInputValue());
+		System.out.println("A3 = " + sp.getCell("A3").getInputValue());
+		System.out.println("B4 = " + sp.getCell("B4").getInputValue());
+	}
 }
